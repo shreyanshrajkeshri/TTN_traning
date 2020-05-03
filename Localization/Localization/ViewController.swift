0d177f4852acf7c155c60005bdb56899aa56673e
shreyanshrajkeshri <shreyansh.raj@tothenew.com>
2020-05-03 19:26:45 +0530

    Adding App Localization File

--- Localization/Localization/ViewController.swift
+++ Localization/Localization/ViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        label.text = NSLocalizedString("MyKey", comment: "App Localization")
+        label.text = NSLocalizedString("TextKey", comment: "App Localization")
+        imageView.image = UIImage(named: NSLocalizedString("ImageKey", comment: "Sample Image"))!
+
     }
 
 
